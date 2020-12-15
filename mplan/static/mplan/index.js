b554aa38c990ca67b07326bf3e35b62e72aa9ca3
Martyanov Andrey <37772440+martyanovandrey@users.noreply.github.com>
2020-12-15 21:44:55 +0300

    post js object 1.2

--- mplan/static/mplan/index.js
+++ mplan/static/mplan/index.js
@@ -91,6 +91,20 @@ function ingredient(ingredientId) {
 	);
 }
 
+function getCookie(name) {
+	if (!document.cookie) {
+		return null;
+	}
+	const token = document.cookie.split(';')
+		.map(c => c.trim())
+		.filter(c => c.startsWith(name + '='));
+
+	if (token.length === 0) {
+		return null;
+	}
+	return decodeURIComponent(token[0].split('=')[1]);
+}
+
 function create_recipe() {
 	fetch('/create_recipe', {
 			credentials: 'include',
